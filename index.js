3f0467f563e351ba899b254248a0d1cdb3c97c14
Matthew Bierman <matthew.bierman2@gmail.com>
2018-08-28 15:42:28 -0500

    Created home page; created home section and made it fit to window height; created navbar with logo and menu

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+
+document.ready -- runs when the DOM is ready, e.g. all elements are there to be found/used, but not necessarily all content
+window.onload -- when images and such are loaded
+
+*/
+
+window.onload = function() {
+    fullSizeHome();
+}
+
+$(window).resize(function() {
+    fullSizeHome();
+});
+
+function fullSizeHome() {
+    $("#home").height($(window).height());
+}
